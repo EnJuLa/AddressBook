dfc1062ef4d7c37862fdece432b4f17db2aca886
mschuma2 <mschuma2@gmail.com>
2014-11-20 22:34:19 +0100

    add contacts from file

--- AddressBook/src/addressbook/FileAddressProvider.java
+++ AddressBook/src/addressbook/FileAddressProvider.java
@@ -3,6 +3,7 @@ package addressbook;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -11,29 +12,40 @@ public class FileAddressProvider extends AddressProvider {
 	public String name = "file";
 
 	public List<Contact> provideAddresses() {
-		return null;
+		return contacts;
 	}
 
 	public String getName() {
 		return name;
 	}
-	public void readContacts() throws IOException{ 	
-		FileReader fr = new FileReader("contacts.csv");
-		BufferedReader br = new BufferedReader(fr);
+	public void readContacts(){
+		FileReader fr = null;
+		BufferedReader br = null;
 		
-		String zeile = "";
+		try {
+			fr = new FileReader("contacts.csv");
+			br = new BufferedReader(fr);
+			while (true){
+				String line = br.readLine();
+				if (line == null)
+					break;
+				System.out.println(line);
+				String [] contactSplitted = line.split(";");
+				Contact contact = new Contact(contactSplitted[0],contactSplitted[1],contactSplitted[2],contactSplitted[3],contactSplitted[4],contactSplitted[5], contactSplitted[6]);
+				contacts.add(contact);
+			}
+		} catch (IOException e){
+			System.err.println("Error2 :" +e);
+		}finally{
+			try{
+				if (br!= null) br.close();
+				if (fr!=null) fr.close();
+			}catch (IOException e){
+				System.err.println("Error2 :" +e);
+			}
 		
-		while (zeile != null){
-			processCsvLine(zeile);
-			zeile = br.readLine();
-		}	
-		br.close();
-	}
-	public processCsvLine(final String data){
-		final StringTokenizer st = new StringTokenizer(data, ";");
-		while (st.hasMoreTokens()){
-			return;
 		}
 	}
+	
 	
 }
